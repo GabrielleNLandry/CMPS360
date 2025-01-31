cde37256850027ae1a9673fa337b2ce65693ccaa
Gabrielle Landry <landry.g.2002@gmail.com>
2025-01-31 12:21:36 -0500

    Added error handeling

--- Module 3/artifact-demo/src/main/java/com/ppu/artifact_demo/HelloController.java
+++ Module 3/artifact-demo/src/main/java/com/ppu/artifact_demo/HelloController.java
@@ -3,6 +3,7 @@ package com.ppu.artifact_demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @RestController
 public class HelloController {
@@ -18,4 +19,11 @@ public class HelloController {
     public String greet(@RequestParam(value = "name", defaultValue = "Guest") String name) {
         return "Hello, " + name + "!";
     }
+
+    // Error handling for invalid requests
+    @ExceptionHandler(Exception.class)
+    public String handleError(Exception ex) {
+        return "Something went wrong: " + ex.getMessage();
+    }
 }
+
