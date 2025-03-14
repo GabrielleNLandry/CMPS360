f889798b4e2fdde5f0a5f853842cdaec28fdcf89
Gabrielle Landry <landry.g.2002@gmail.com>
2025-03-14 16:18:29 -0400

    added hobby and about pages

--- Module9/go-website/main.go
+++ Module9/go-website/main.go
@@ -6,6 +6,24 @@ import (
 	"net/http"
 )
 
+func hobbyHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/hobby.html")
+	if err != nil {
+		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/about.html")
+	if err != nil {
+	http.Error(w, "Error loading template", http.StatusInternalServerError)
+	return
+	}
+	tmpl.Execute(w, nil)
+	}
+
 //Handler Function for the Homepage
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("templates/index.html")
@@ -17,10 +35,16 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler) //Route for the homepage
-	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", homeHandler) // Route for the homepage
+	http.HandleFunc("/about", aboutHandler) // Route for the about page
+	http.HandleFunc("/hobby", hobbyHandler) // Route for the hobby page
 
+	// Serve static files from the "static" directory
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	log.Println("Server is runnning on http://localhost:8080")
-	http.ListenAndServe(":8080", nil) //Start the server on port 8080
+	log.Println("Server is running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil) // Start the server on port 8080
+	if err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
 }
